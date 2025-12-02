bcf109d827641979c2022b13169b2f4ef7a591c6
omar embaby <omarembaby39@gmail.com>
2025-12-02 16:36:36 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,26 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from config import DB_URL
+
+def get_conn():
+    return psycopg2.connect(DB_URL, cursor_factory=RealDictCursor)
+
+def init_acc_tables():
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS acc_cash_transactions (
+        id SERIAL PRIMARY KEY,
+        tran_date DATE NOT NULL,
+        project_code TEXT,
+        tran_type TEXT NOT NULL CHECK (tran_type IN ('IN','OUT')),
+        category TEXT,
+        description TEXT,
+        amount NUMERIC(14,2) NOT NULL
+    );
+    """)
+
+    conn.commit()
+    conn.close()
 

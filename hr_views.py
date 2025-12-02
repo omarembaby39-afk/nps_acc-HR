34c4dcab20fd57ee0980c11f086b0ec611d8842c
omar embaby <omarembaby39@gmail.com>
2025-12-02 16:37:44 +0300

    Update hr_views.py

--- hr_views.py
+++ hr_views.py
@@ -1,1 +1,31 @@
+from db import get_conn
+
+def fetch_employees_for_accounting():
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM employees_for_accounting ORDER BY emp_code;")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def fetch_projects_for_accounting():
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM projects_for_accounting ORDER BY project_code;")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def fetch_payroll_for_month(month_str: str):
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT *
+        FROM payroll_for_accounting
+        WHERE month = %s
+        ORDER BY worker_code;
+    """, (month_str,))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
 

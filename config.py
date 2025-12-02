c037876d46fea0a18e12e088425fdf8d1947b107
omar embaby <omarembaby39@gmail.com>
2025-12-02 16:37:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+import os
+
+DB_URL = os.getenv(
+    "NPS_DB_URL",
+    "postgresql://neondb_owner:npg_C4ghxK1yUcfw@ep-billowing-fog-agxbr2fc-pooler.c-2.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+)
+
+APP_TITLE = "NPS Accounting System"
 
